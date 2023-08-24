58ee3dd84d4c63e27a6d34e9a6814dedfe9898b1
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-24 17:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-var line = document.getElementById("line1");
-var centerX = 60, centerY = 100, radius = 100, angle = 0;
-var rotate = function(angle){
-    var x1, y1, x2, y2;
- 	x1 = centerX + radius * Math.cos( angle );
-    y1 = centerY + radius * Math.sin( angle );
-    x2 = centerX - radius * Math.cos( angle );
-    y2 = centerY - radius * Math.sin( angle );
-    
-    line.setAttribute( 'x1', x1 );
-    line.setAttribute( 'y1', y1 );
-    line.setAttribute( 'x2', x2 );
-    line.setAttribute( 'y2', y2 );
-}
-// animation
-setInterval( function(){
-    angle += .1;
-    rotate( angle );
-},100);
+const line = document.getElementById('line');
+let rotation = 0;
+ 
+setInterval(() => {
+  rotation += 5;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 50);
